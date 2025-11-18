dc4d79f04a61f913278dd3e7885be3e604755785
tausePro <felipe@tause.co>
2025-11-18 11:42:36 -0500

    fix: Hacer vista de error robusta cuando $setting no está disponible
    - Verificar que $setting existe antes de usarlo
    - Usar valores por defecto para evitar errores cuando la BD no está disponible
    - Esto resuelve el error 'Undefined variable $setting' en staging

--- resources/views/default/layout/error.blade.php
+++ resources/views/default/layout/error.blade.php
@@ -3,31 +3,31 @@
 @section('content')
     <header class="fixed inset-x-0 top-0 flex justify-center border-b bg-background/10 px-4 py-6 backdrop-blur-md">
         <figure>
-            @if (isset($setting->logo_dashboard))
+            @if (isset($setting) && isset($setting->logo_dashboard))
                 <img
                     class="max-h-14 w-auto dark:hidden"
                     src="{{ custom_theme_url($setting->logo_dashboard_path, true) }}"
                     @if (isset($setting->logo_dashboard_2x_path)) srcset="/{{ $setting->logo_dashboard_2x_path }} 2x" @endif
-                    alt="{{ $setting->site_name }}"
+                    alt="{{ $setting->site_name ?? 'Logo' }}"
                 >
                 <img
                     class="hidden max-h-14 w-auto dark:block"
                     src="{{ custom_theme_url($setting->logo_dashboard_dark_path, true) }}"
                     @if (isset($setting->logo_dashboard_dark_2x_path)) srcset="/{{ $setting->logo_dashboard_dark_2x_path }} 2x" @endif
-                    alt="{{ $setting->site_name }}"
+                    alt="{{ $setting->site_name ?? 'Logo' }}"
                 >
-            @else
+            @elseif (isset($setting))
                 <img
                     class="max-h-14 w-auto dark:hidden"
-                    src="{{ custom_theme_url($setting->logo_path, true) }}"
+                    src="{{ custom_theme_url($setting->logo_path ?? '', true) }}"
                     @if (isset($setting->logo_2x_path)) srcset="/{{ $setting->logo_2x_path }} 2x" @endif
-                    alt="{{ $setting->site_name }}"
+                    alt="{{ $setting->site_name ?? 'Logo' }}"
                 >
                 <img
                     class="hidden max-h-14 w-auto dark:block"
-                    src="{{ custom_theme_url($setting->logo_dark_path, true) }}"
+                    src="{{ custom_theme_url($setting->logo_dark_path ?? '', true) }}"
                     @if (isset($setting->logo_dark_2x_path)) srcset="/{{ $setting->logo_dark_2x_path }} 2x" @endif
-                    alt="{{ $setting->site_name }}"
+                    alt="{{ $setting->site_name ?? 'Logo' }}"
                 >
             @endif
         </figure>
